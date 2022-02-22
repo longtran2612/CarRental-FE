2df0dea188fc15c8491e8586c09140302269125d
Your Name <email@example.com>
2022-02-22 23:13:45 +0700

    Modal change password

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'antd'
+import ModalChangePassword from '../../components/ModalChangePassword/ModalChangePassword';
 
 export default function Profile() {
+
+  const [showModalChangePassword, setshowModalChangePassword] = useState(false);
+
+  const handleClose = () => setshowModalChangePassword(false);
+  const handleShow = () => setshowModalChangePassword(true);
+
+
   return (
-    <div>day la profile</div>
+    <div className='profile'>
+
+
+      <Button variant="primary" onClick={handleShow}>
+       changepassword
+      </Button>
+      {showModalChangePassword && <ModalChangePassword visible ={showModalChangePassword} onCancel={!showModalChangePassword}   />}
+
+
+    </div>
   )
 }
